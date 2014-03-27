2331d135f80505e5a26434220ed02025fb55916b
fernandacoto <fermarioro@gmail.com>
2014-03-27 12:31:44 -0500

    Ordering location where images are going to appear

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -17,32 +17,45 @@ private
     when 2
       move_from_loading_deck_to_product_line(option_move[:stack])
     when 3
-      move_from_feeder_line_to_feeder_line(option_move[:stack])
+      move_from_feeder_line_to_feeder_line(option_move)
     when 4
-      move_from_feeder_line_to_product_line(option_move[:stack])
+      move_from_feeder_line_to_product_line(option_move)
     when 5
-      move_from_product_line_to_feeder_line(option_move[:stack])
+      move_from_product_line_to_feeder_line(option_move)
     end
   end
 
   def move_from_loading_deck_to_feeder_line(movement)
-
+    if valid_movement_feeder_line? movement
+    end
   end
 
   def move_from_loading_deck_to_product_line(movement)
-
+    if valid_movement_product_line? movement
+    end
   end
 
   def move_from_feeder_line_to_feeder_line(movement)
-
+    if valid_movement_feeder_line? movement[:stack_number_init] and valid_movement_feeder_line? movement[:stack_number_destiny]
+    end
   end
 
   def move_from_feeder_line_to_product_line(movement)
-
+    if valid_movement_feeder_line? movement[:stack_number_init] and valid_movement_product_line? movement[:stack_number_destiny]
+    end
   end
 
   def move_from_product_line_to_feeder_line(movement)
+    if valid_movement_feeder_line? movement[:stack_number_destiny] and valid_movement_product_line? movement[:stack_number_init]
+    end
+  end
 
+  def valid_movement_feeder_line?(movement)
+    movement.between?(1,7)
+  end
+
+  def valid_movement_product_line?(movement)
+    movement.between?(1,4)
   end
 
 end
