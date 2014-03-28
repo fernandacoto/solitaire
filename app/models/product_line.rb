c4e6d2db1c1b3183063668aaad67b408a1aa1c57
satchiss08 <satchiss08@gmail.com>
2014-03-27 19:59:37 -0500

    Loading Deck to Product Line Movement

--- app/models/product_line.rb
+++ app/models/product_line.rb
@@ -1,5 +1,5 @@
 class ProductLine
-  attr_accessor :product_line, :feeder_deck
+  attr_accessor :product_line, :feeder_deck, :loading_deck
 
   def initialize
   	@product_line = []
@@ -21,6 +21,20 @@ class ProductLine
     feeder_deck[feeder_move].delete_at(feeder_line_size - 1)
     if !feeder_deck[feeder_move].empty?
       feeder_deck[feeder_move][feeder_line_size - 1].visible = 1
+    end
+  end
+
+  def move_from_loading_deck(loading_move,product_move,loading_deck)
+  	loading_deck_size = loading_deck.size
+    product_line_size = @product_line[product_move].size
+    if !@product_line[product_move].empty?
+      @product_line[product_move][product_line_size - 1].visible = 0	
+    end
+    loading_deck[loading_deck_size - 1].state = "PL"
+    @product_line[product_move] << loading_deck[loading_deck_size - 1]
+    loading_deck.delete_at(loading_deck_size - 1)
+    if !loading_deck.empty?
+      loading_deck[loading_deck_size - 1].visible = 1
     end
   end
 end
