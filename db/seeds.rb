26ad701f18e23e600ae077a3878f3b8c6895da5b
fernandacoto <fermarioro@gmail.com>
2014-03-31 10:47:18 -0500

    Adding cards to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Game.create
+
+Deck.create(game_id: 1)
+
+cards_spades = 2
+Card.create(number: "A", suit: "Spades", in_use: 0, deck_id: 1)
+while cards_spades < 11
+  Card.create(number: cards_spades.to_s, suit: "Spades", in_use: 0, deck_id: 1)
+  cards_spades += 1
+end
+Card.create(number: "J", suit: "Spades", in_use: 0, deck_id: 1)
+Card.create(number: "Q", suit: "Spades", in_use: 0, deck_id: 1)
+Card.create(number: "K", suit: "Spades", in_use: 0, deck_id: 1)
+
+cards_club = 2
+Card.create(number: "A", suit: "Club", in_use: 0, deck_id: 1)
+while cards_club < 11
+  Card.create(number: cards_club.to_s, suit: "Club", in_use: 0, deck_id: 1)
+  cards_club += 1
+end
+Card.create(number: "J", suit: "Club", in_use: 0, deck_id: 1)
+Card.create(number: "Q", suit: "Club", in_use: 0, deck_id: 1)
+Card.create(number: "K", suit: "Club", in_use: 0, deck_id: 1)
+
+cards_hearts = 2
+Card.create(number: "A", suit: "Hearts", in_use: 0, deck_id: 1)
+while cards_hearts < 11
+  Card.create(number: cards_hearts.to_s, suit: "Hearts", in_use: 0, deck_id: 1)
+  cards_hearts += 1
+end
+Card.create(number: "J", suit: "Hearts", in_use: 0, deck_id: 1)
+Card.create(number: "Q", suit: "Hearts", in_use: 0, deck_id: 1)
+Card.create(number: "K", suit: "Hearts", in_use: 0, deck_id: 1)
+
+cards_diamonds = 2
+Card.create(number: "A", suit: "Diamonds", in_use: 0, deck_id: 1)
+while cards_diamonds < 11
+  Card.create(number: cards_diamonds.to_s, suit: "Diamonds", in_use: 0, deck_id: 1)
+  cards_diamonds += 1
+end
+Card.create(number: "J", suit: "Diamonds", in_use: 0, deck_id: 1)
+Card.create(number: "Q", suit: "Diamonds", in_use: 0, deck_id: 1)
+Card.create(number: "K", suit: "Diamonds", in_use: 0, deck_id: 1)
